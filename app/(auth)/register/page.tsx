8eb1a445e880010ba3b8e4445fdff6739cfcf5df
viniciosragazzidev <viniciosragazzidev@gmail.com>
2024-05-29 13:26:25 -0300

    initial commit

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import RegisterForm from "./components/register-form";
 import Image from "next/image";
-import Ignix from "@/shared/assets/images/ignix.svg";
+import ignix from "@/public/images/ignix.svg";
+
 const Register = () => {
   return (
     <div className="w-full min-h-screen border-b-2 border-primary  flex flex-col justify-center">
       <span className="flex justify-center lg:absolute   top-0  left-10 mt-6 max-lg:mb-6 font-bold text-xl max-lg:text-3xl items-center gap-1 ">
         <Image
-          src={Ignix}
+          src={ignix}
           alt="logo"
           width={24}
           height={24}
