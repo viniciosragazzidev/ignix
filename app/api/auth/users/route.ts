dce46a433d0cff2365c08e93e8a9658bc019f331
viniciosragazzidev <viniciosragazzidev@gmail.com>
2024-05-29 16:42:15 -0300

    Feat: News Models in Schema Prisma

--- app/api/auth/users/route.ts
+++ app/api/auth/users/route.ts
@@ -1,1 +1,6 @@
-export async function GET(response: Response, request: Request) {}
+import db from "@/services/db";
+
+export async function GET(response: Response, request: Request) {
+  const users = await db.user.findMany();
+  return new Response(JSON.stringify(users), { status: 200 });
+}
