15e170376108779fd214557a1279d90ad0df25e4
viniciosragazzidev <viniciosragazzidev@gmail.com>
2024-05-29 16:44:23 -0300

    :sparkles: feat: New Models in schema

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,7 @@
 import db from "@/services/db";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(response: Response, request: Request) {
+export async function GET(response: NextResponse, request: NextRequest) {
   const users = await db.user.findMany();
   return new Response(JSON.stringify(users), { status: 200 });
 }
